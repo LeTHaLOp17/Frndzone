4edbd54d074ddb81730d461b0b4a147ffa179a9a
Ayush Kumar Patel <aksingh6224@gmail.com>
2024-08-05 23:13:14 +0530

    Loader Sign up image

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//Loader Image when we press Sign up button
 const Loader = () => {
   return (
     <div className='flex-center w-full'>
@@ -7,7 +8,7 @@ const Loader = () => {
       src="/assets/icons/loader.svg"
       alt="loader"
       width={24}
-      height={24}
+      height={24   }
       />
     </div>
   )
